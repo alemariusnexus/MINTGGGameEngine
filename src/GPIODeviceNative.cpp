b22c4b4e0663924263a6ffeb3fbe44d8f8688b23
alemariusnexus <david@dlerch.de>
2026-03-02 14:52:06 +0100

    Fix GPIODeviceNative recursion

--- src/GPIODeviceNative.cpp
+++ src/GPIODeviceNative.cpp
@@ -14,18 +14,18 @@ GPIODeviceNative& GPIODeviceNative::getInstance()
 
 bool GPIODeviceNative::setPinMode(unsigned int pin, uint8_t mode)
 {
-	pinMode(static_cast<uint8_t>(pin), mode);
+	::pinMode(static_cast<uint8_t>(pin), mode);
 	return true;
 }
 
 uint8_t GPIODeviceNative::readPin(unsigned int pin)
 {
-	return digitalRead(pin);
+	return ::digitalRead(pin);
 }
 
 void GPIODeviceNative::writePin(unsigned int pin, uint8_t val)
 {
-	digitalWrite(pin, val);
+	::digitalWrite(pin, val);
 }
 
 
